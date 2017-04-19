752b606db757b2121f1b9c79f30d874f6d7b1275
Faizal <faizal.hussain33@gmail.com>
2017-04-18 22:16:00 -0400

    Completion of Assignment2.js

--- assignment_2/assignment2.js
+++ assignment_2/assignment2.js
@@ -2,8 +2,9 @@
 Exercise #1
 Create a for loop that prints out the numbers 1 to 100 in the console.
 */
-
-
+    for (var count = 0; count <= 100; count ++) {
+        console.log (count);
+    }
 
 /*
 Exercise #2
@@ -17,3 +18,6 @@ Write a loop that makes seven calls to console.log to output the following trian
 ######
 #######
 */
+for (var triangle = "#"; triangle.length < 8; triangle += "#"){
+    console.log(triangle);
+}
